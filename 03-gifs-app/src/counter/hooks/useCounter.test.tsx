698be671609bb7dff7d4209b8bd26dc5c0a1e1ee
sandfall74 <adrianpiedrasan@gmail.com>
2025-08-27 23:59:54 -0600

    avance en test

---
+++ 03-gifs-app/src/counter/hooks/useCounter.test.tsx
@@ -0,0 +1,26 @@
+import { act, renderHook } from "@testing-library/react";
+import { beforeEach, describe, expect, test } from "vitest";
+import { useCounter } from "./useCounter";
+
+
+
+describe('useCounter', () => {
+    /* let result;
+    beforeEach(() => {
+        const {result:hookValue} = renderHook(() => useCounter());
+        result = hookValue;
+    }); */
+    test('should initialize with default value of 10', () => {
+        const  {result}  = renderHook(()=>useCounter());
+        expect(result.current.Counter).toBe(10);
+   });    
+   test('should increment counter when handleAdd', () => {
+        const  {result}  = renderHook(()=>useCounter());
+        act(()=>{result.current.handleAdd()});
+        expect(result.current.Counter).toBe(11);
+   });    
+}       );
+
+
+
+
